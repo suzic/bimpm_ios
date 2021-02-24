86f3b7ea1b5a634ab5f018c0fd29ad7568b9bb46
liuchao <>
2021-02-24 11:06:00 +0800

    调整获取用户信息顺序

--- TTManager/Modules/ConversationController/IMDelegateManager/IMDelegateManager.m
+++ TTManager/Modules/ConversationController/IMDelegateManager/IMDelegateManager.m
@@ -34,22 +34,23 @@
             completion(userInfo);
         }
     }
-    // 2:从本地数据库查询
-    ZHUser *user = [[LoginUserManager defaultInstance] getUserByRongIMId:userId];
-    if (user && ![SZUtil isEmptyOrNull:user.name]) {
-        RCUserInfo *userInfo = [[RCUserInfo alloc] init];
-        userInfo.name = user.name;
-        userInfo.userId = [NSString stringWithFormat:@"%d",user.id_user];
-        userInfo.portraitUri = user.avatar;
-        completion(userInfo);
-    }
-    // 3:从服务器获取用户信息
     else{
-        [RCDUserInfoManager getUserInfoFromServer:userId
-                                             complete:^(RCUserInfo *userInfo) {
-                                                    completion(userInfo);
-                
-            }];
+        // 2:从本地数据库查询
+        ZHUser *user = [[LoginUserManager defaultInstance] getUserByRongIMId:userId];
+        if (user && ![SZUtil isEmptyOrNull:user.name]) {
+            RCUserInfo *userInfo = [[RCUserInfo alloc] init];
+            userInfo.name = user.name;
+            userInfo.userId = [NSString stringWithFormat:@"%d",user.id_user];
+            userInfo.portraitUri = user.avatar;
+            completion(userInfo);
+        }else{
+            // 3:从服务器获取用户信息
+            [RCDUserInfoManager getUserInfoFromServer:userId
+                                                 complete:^(RCUserInfo *userInfo) {
+                                                        completion(userInfo);
+                    
+                }];
+        }
     }
     [[RCIM sharedRCIM] refreshUserInfoCache:userInfo withUserId:userId];
 }
